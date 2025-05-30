7121fd7d32800153478087e38e9a9be31388377a
Simone Suma <simo.s.240802@gmail.com>
2025-05-30 13:14:12 +0200

    Connection package completed

--- src/main/java/com/smartVend/app/model/connection/Connection.java
+++ src/main/java/com/smartVend/app/model/connection/Connection.java
@@ -1,5 +1,31 @@
 package com.smartVend.app.model.connection;
 
-public class Connection {
-    
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Connection implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String userId;
+
+    @Column(nullable = false)
+    public String machineId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    public Date start;
+
+    public Connection() {}
+
+    public Connection(String userId, String machineId, Date start) {
+        this.userId = userId;
+        this.machineId = machineId;
+        this.start = start;
+    }
 }
