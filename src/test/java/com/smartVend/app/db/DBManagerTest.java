924408c6624bc53e7df46f349308dddd527534c4
Matteo Minin <matteominin@MBP-di-Matteo-2.fritz.box>
2025-05-30 16:48:26 +0200

    Pom dependencies fix

--- src/test/java/com/smartVend/app/db/DBManagerTest.java
+++ src/test/java/com/smartVend/app/db/DBManagerTest.java
@@ -13,7 +13,6 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.anyString;
